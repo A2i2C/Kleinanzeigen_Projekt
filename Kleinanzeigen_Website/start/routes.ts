e60d059f429f093e4e2ac26eb44c255a556952ec
Aziz Carducci <inf22009@lehre.dhbw-stuttgart.de>
2024-04-03 03:44:55 +0200

    formatted pages added Registrierungs-page and route login and registrierung text not centered

--- Kleinanzeigen_Website/start/routes.ts
+++ Kleinanzeigen_Website/start/routes.ts
@@ -11,11 +11,14 @@ import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home')
 
-router.get('/home', async({ view })=>{
-    return view.render('pages/home')
+router.get('/home', async ({ view }) => {
+  return view.render('pages/home')
 })
 
+router.get('/login', async ({ view }) => {
+  return view.render('pages/login')
+})
 
-router.get('/login', async({ view })=>{
-    return view.render('pages/login')
+router.get('/registrierung', async ({ view }) => {
+  return view.render('pages/registrierung')
 })
