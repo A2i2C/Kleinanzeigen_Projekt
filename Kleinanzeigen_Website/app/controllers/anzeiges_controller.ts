0f86ca79dda4a2d799b438e9678f8f460a382cb3
Aziz Carducci <carducciaziz@gmail.com>
2024-05-02 14:24:02 +0200

    Favoriten können gespeichert werden

--- Kleinanzeigen_Website/app/controllers/anzeiges_controller.ts
+++ Kleinanzeigen_Website/app/controllers/anzeiges_controller.ts
@@ -2,93 +2,156 @@ import { cuid } from '@adonisjs/core/helpers'
 import type { HttpContext } from '@adonisjs/core/http'
 import app from '@adonisjs/core/services/app'
 import db from '@adonisjs/lucid/services/db'
-
+import { test } from '@japa/runner'
 
 export default class AnzeigesController {
+  public async index({ view, session }: HttpContext) {
+    const item = await db.from('Items').select('*')
+    const itemImages = await db
+      .from('itemImages')
+      .select('*')
+      .join('Items', 'Items.itemid', 'itemImages.itemID')
+      .groupBy('Items.itemid')
+    return view.render('pages/home', { item, itemImages, current_user: session.get('user') })
+  }
 
-    public async index({ view, session }: HttpContext) {
-        const item = await db.from('Items').select('*')
-        const itemImages = await db.from('itemImages').select('*').join('Items', 'Items.itemid', 'itemImages.itemID').groupBy('Items.itemid')
-        return view.render('pages/home', { item, itemImages, current_user: session.get('user') })
+  async createForm({ view, session }: HttpContext) {
+    const current_user = session.get('user')
+    if (!current_user) {
+      return view.render('pages/user/login')
+    }
+    return view.render('pages/anzeigen/anzeigeaufgeben', { current_user: session.get('user') })
+  }
+
+  async show_site({ view, session, params }: HttpContext) {
+    const item = await db.from('Items').select('*').where('itemID', params.itemID).first()
+    const itemImages = await db.from('itemImages').select('*').where('itemID', params.itemID)
+    const user = await db.from('user').select('*').where('email', item.email).first()
+    const current_user = session.get('user')
+    return view.render('pages/anzeigen/anzeigeseite', { item, itemImages, user, current_user })
+  }
+
+  async createProcess({ request, response, session, view }: HttpContext) {
+    const images = request.files('images', { size: '2mb', extnames: ['jpg', 'png', 'jpeg'] })
+
+    if (!images || !images[0]) {
+      return view.render('pages/anzeigen/anzeigeaufgeben', { error: 'Bitte Bilder hochladen' })
     }
 
-    async createForm({ view, session }: HttpContext) {
-
-        const current_user = session.get('user')
-        if (!current_user) {
-            return view.render('pages/user/login')
-        }
-        return view.render('pages/anzeigen/anzeigeaufgeben', { current_user: session.get('user') })
-
+    for (const image of images) {
+      await image.move(app.publicPath('images'), { name: `${cuid()}.${image.extname}` })
     }
 
-    async show_site({ view, session, params }: HttpContext) {
-        const item = await db.from('Items').select('*').where('itemID', params.itemID).first()
-        const itemImages = await db.from('itemImages').select('*').where('itemID', params.itemID)
-        const user = await db.from('user').select('*').where('email', item.email).first()
-        const current_user = session.get('user')
-        console.log(user)
-        return view.render('pages/anzeigen/anzeigeseite', { item, itemImages, user, current_user })
+    const current_user = session.get('user')
+    if (!current_user) {
+      return response.redirect('/login')
     }
 
-    async createProcess({ request, response, session, view }: HttpContext) {
+    const verhandelbar = request.input('negotiable') === 'Ja' ? 'Ja' : 'Nein'
+    const versand =
+      request.input('shipping') === 'Nein'
+        ? ''
+        : request.input('shipping_price') === ''
+          ? 0.0
+          : request.input('shipping_price')
 
-        const images = request.files('images', { size: '2mb', extnames: ['jpg', 'png', 'jpeg'] })
-
-        if (!images || !images[0]) {
-            return view.render('pages/anzeigen/anzeigeaufgeben', { error: 'Bitte Bilder hochladen' })
-        }
-
-        for (const image of images) {
-            await image.move(app.publicPath('images'), { name: `${cuid()}.${image.extname}` })
-        }
-
-        const current_user = session.get('user')
-        if (!current_user) {
-            return response.redirect('/login')
-        }
-
-        const verhandelbar = request.input('negotiable') === 'Ja' ? "Ja" : "Nein"
-        const versand = request.input('shipping') === 'Nein' ? '' : request.input('shipping_price') === '' ? 0.0 : request.input('shipping_price')
-        
-        try {
-            const anzeige = await db.table('Items').insert({
-                itemName: request.input('title'),
-                kategorie: request.input('category'),
-                preis: request.input('price'),
-                beschreibung: request.input('description'),
-                verhandelbar: verhandelbar,
-                versand: versand,
-                email: current_user.email
-            })
-            for (const image of images) {
-                await db.table('itemImages').insert({
-                    bildReferenz: image.fileName,
-                    itemID: anzeige[0]
-                })
-            }
-        }
-        catch (error) { return error }
-
-        response.redirect('/')
+    try {
+      const anzeige = await db.table('Items').insert({
+        itemName: request.input('title'),
+        kategorie: request.input('category'),
+        preis: request.input('price'),
+        beschreibung: request.input('description'),
+        verhandelbar: verhandelbar,
+        versand: versand,
+        email: current_user.email,
+      })
+      for (const image of images) {
+        await db.table('itemImages').insert({
+          bildReferenz: image.fileName,
+          itemID: anzeige[0],
+        })
+      }
+    } catch (error) {
+      return error
     }
 
-    async youritems({ view, session }: HttpContext) {
-        const current_user = session.get('user')
-        if (!current_user) {
-            return view.render('pages/user/login')
-        }
+    response.redirect('/')
+  }
 
-        console.log(current_user.email)
-
-        const item = await db.from('Items').select('*').where('email', current_user.email)
-        console.log(item[0])
-        const itemImages = await db
-          .from('itemImages')
-          .select('*')
-          .join('Items', 'Items.itemid', 'itemImages.itemID')
-          .where('Items.email', current_user.email)
-          .groupBy('Items.itemid')
-        return view.render('pages/user/userprofile_items', { item, itemImages, current_user: session.get('user') })
+  async youritems({ view, session }: HttpContext) {
+    const current_user = session.get('user')
+    if (!current_user) {
+      return view.render('pages/user/login')
     }
+
+    console.log(current_user.email)
+
+    const item = await db.from('Items').select('*').where('email', current_user.email)
+    console.log(item[0])
+    const itemImages = await db
+      .from('itemImages')
+      .select('*')
+      .join('Items', 'Items.itemid', 'itemImages.itemID')
+      .where('Items.email', current_user.email)
+      .groupBy('Items.itemid')
+    return view.render('pages/user/userprofile_items', {
+      item,
+      itemImages,
+      current_user: session.get('user'),
+    })
+  }
+
+  async favorites({ view, session }: HttpContext) {
+    const current_user = session.get('user')
+    if (!current_user) {
+      return view.render('pages/user/login')
+    }
+
+    console.log(current_user.email)
+    console.log(await db.from('Items').select('*').where('email', current_user.email))
+
+    const favorites = await db
+      .from('favorisierteItems')
+      .select('*')
+      .where('email', current_user.email)
+      .groupBy('itemID')
+
+    const itemIDs = favorites.map((favorite) => favorite.itemID)
+
+    const items = await db.from('Items').select('*').whereIn('itemID', itemIDs)
+    const itemImages = await db
+      .from('itemImages')
+      .select('*')
+      .join('Items', 'Items.itemid', 'itemImages.itemID')
+      .whereIn('Items.itemid', itemIDs)
+      .groupBy('Items.itemid')
+
+    console.log(favorites)
+
+    return view.render('pages/user/userprofile_favorites', {
+      items,
+      itemImages,
+      favorites,
+      current_user: session.get('user'),
+    })
+  }
+
+  async createFavorite({ view, request, response, session }: HttpContext) {
+    const current_user = session.get('user')
+    if (!current_user) {
+      return response.redirect('/login')
+    }
+
+      const item = await db.from('Items').select('itemID').where('itemID', request.input('itemID'))
+
+    try {
+      await db.table('favorisierteItems').insert({
+        itemID: item[0].itemID,
+        email: current_user.email,
+      })
+    } catch (error) {
+      return view.render('pages/anzeige/anzeigeseite:itemId', { error: 'Item schon Favorisiert' })
+    }
+    response.redirect('/pages/anzeige/anzeigeseite:itemId')
+  }
 }
--- Kleinanzeigen_Website/start/routes.ts
+++ Kleinanzeigen_Website/start/routes.ts
@@ -12,17 +12,18 @@ import router from '@adonisjs/core/services/router'
 import UsersController from '../app/controllers/users_controller.js'
 import AnzeigesController from '../app/controllers/anzeiges_controller.js'
 
-
-router.get('/' , [AnzeigesController, 'index'])
+router.get('/', [AnzeigesController, 'index'])
 
 router.get('/anzeigeaufgeben', [AnzeigesController, 'createForm'])
 router.post('/anzeigeaufgeben', [AnzeigesController, 'createProcess'])
 router.get('/anzeigeseite/:itemID', [AnzeigesController, 'show_site'])
 
+router.get('/youritems', [AnzeigesController, 'youritems'])
+router.post('/', [AnzeigesController, 'createFavorite'])
+router.get('/favorites', [AnzeigesController, 'favorites'])
+
 router.get('/userprofile', [UsersController, 'userprofile'])
 router.post('/userprofile', [UsersController, 'updateProfile'])
-
-router.get('/youritems', [AnzeigesController, 'youritems'])
 
 router.get('/registrierung', [UsersController, 'registerForm'])
 router.post('/registrierung', [UsersController, 'registerProcess'])
@@ -30,4 +31,3 @@ router.post('/registrierung', [UsersController, 'registerProcess'])
 router.get('/login', [UsersController, 'loginForm'])
 router.post('/login', [UsersController, 'loginProcess'])
 router.get('/logout', [UsersController, 'logout'])
-
