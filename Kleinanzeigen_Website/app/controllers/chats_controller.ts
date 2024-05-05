3c2c65da03262cc4339826570b506249e5ab01f0
Aziz Carducci <carducciaziz@gmail.com>
2024-05-05 23:11:12 +0200

    Link goes to chat now

--- Kleinanzeigen_Website/app/controllers/chats_controller.ts
+++ Kleinanzeigen_Website/app/controllers/chats_controller.ts
@@ -2,39 +2,26 @@ import type { HttpContext } from '@adonisjs/core/http'
 import db from '@adonisjs/lucid/services/db'
 
 export default class ChatsController {
-
-  public async chat({ view, session, params }: HttpContext) {
+  public async chat({ view, params, session }: HttpContext) {
     const current_user = session.get('user')
-    if (current_user === undefined) {
+    const item = await db.from('Items').where('itemID', params.itemID).first()
+    const existingChat = await db.from('Chats').where('itemID', params.itemID).first()
+    if (!current_user) {
       return view.render('pages/user/login')
     }
+    const itemID = params.itemID
 
-
-
-    //Hier wird die Nachrichtenliste erstellt und die verschiedenen Datenbanken einbinden
-
-    return view.render('pages/chats/chattemplate')
-  }
-
-  public async createMessage({ request, response, session, params }: HttpContext) {
-    const current_user = session.get('user')
-    if (current_user === undefined) {
-      return response.redirect('/login')
+    if (existingChat) {
+      return view.render('pages/chats/chat', { itemID })
     }
-    const message = request.input('message')
-    const item = await db.from('Items').where('itemID', params.itemID).first()
 
     await db.table('Chats').insert({
+      itemID: itemID,
       empfaengerID: item.email,
       senderID: current_user.email,
-      itemID: params.itemID,
-      nachrichten: message
-    }) 
-
-    //Fertig mit postposten
-
-    return response.redirect('/chat')
+    })
+    return view.render('pages/chats/chat', { itemID })
   }
 
-
 }
+  
