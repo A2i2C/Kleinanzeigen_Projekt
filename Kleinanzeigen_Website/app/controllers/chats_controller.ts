99b66e025bf645eb99012a570fa0ca46f6a44e0e
Aziz Carducci <carducciaziz@gmail.com>
2024-05-08 01:17:10 +0200

    chore: Update header styling and chat layout and chat-logic

--- Kleinanzeigen_Website/app/controllers/chats_controller.ts
+++ Kleinanzeigen_Website/app/controllers/chats_controller.ts
@@ -8,41 +8,60 @@ export default class ChatsController {
     const item = await db.from('Items').where('itemID', params.itemID).first()
     const existingChat = await db.from('Chats').where('itemID', params.itemID).first()
     const itemID = params.itemID
-    const messagessender = await db.from('user')
+    const messagessender = await db
+      .from('user')
       .join('Nachrichten', 'Nachrichten.absender', 'user.benutzername')
       .join('Chats', 'Chats.chatID', 'Nachrichten.chatID')
       .select('*')
-    
+
+    console.log(messagessender[1].absender === current_user.benutzername)
+    console.log(current_user.benutzername)
+    console.log(messagessender[1].absender === 'tester')
+
     if (!current_user) {
       return view.render('pages/user/login')
     }
 
     if (existingChat) {
-      return view.render('pages/chats/chat', { itemID, current_user, messagessender })
+      if (
+        !existingChat.empfaengerID === current_user.email ||
+        !existingChat.senderID === current_user.email
+      ) {
+        return view.render('/', { error: 'Sie haben keine Berechtigung, diesen Chat zu öffnen' })
+      }
+      return view.render('pages/chats/chat', {
+        item,
+        current_user,
+        messagessender,
+      })
     }
-
-    await db.table('Chats').insert({
-      itemID: itemID,
-      empfaengerID: item.email,
-      senderID: current_user.email,
-    })
 
     return view.render('pages/chats/chat', { itemID })
   }
 
   async createMessage({ request, response, params, session }: HttpContext) {
     const current_user = session.get('user')
+    const item = await db.from('Items').where('itemID', params.itemID).first()
+    
+    await db.table('Chats').insert({
+      itemID: params.itemID,
+      empfaengerID: item.email,
+      senderID: current_user.email,
+    })
+    
     const chatID = await db.from('Chats').where('itemID', params.itemID).first()
     const message = request.input('message')
-    const item = await db.from('Items').where('itemID', params.itemID).first()
+
+
     const date = new Date()
     if (date.getMonth() >= 4 && date.getMonth() <= 11) {
       date.setHours(date.getHours() + 2)
-    }
-    else {
+    } else {
       date.setHours(date.getHours() + 1)
     }
-    const formattedDate = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}: ${date.getHours()}:${date.getMinutes()}`
+
+    const formattedDate = `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()} ${date.getHours() < 10 ? '0' + date.getHours() : date.getHours()}:${date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()}`
+    console.log(formattedDate)
 
     await db.table('Nachrichten').insert({
       chatID: chatID.chatID,
