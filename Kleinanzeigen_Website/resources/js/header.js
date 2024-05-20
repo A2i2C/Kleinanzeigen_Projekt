03e33158ed77cf16accda21a768bf74922fd726f
Aziz Carducci <carducciaziz@gmail.com>
2024-05-21 00:48:38 +0200

    A small Change to contacts

--- Kleinanzeigen_Website/resources/js/header.js
+++ Kleinanzeigen_Website/resources/js/header.js
@@ -1,8 +1,8 @@
 const toggleButton = document.getElementById('toggleNavbar')
-const navbar = document.getElementById('navigationToggle') // You can use getElementById if you prefer an id for the navbar
+const navbar = document.getElementById('navigationToggle')
+
 toggleButton.addEventListener('click', () => {
   if (toggleButton) {
     navbar.classList.toggle('hidden')
   }
-  console.log('clicked')
 })
